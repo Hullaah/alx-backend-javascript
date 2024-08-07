aafa2b895a19282fb7ba59a01f6916fe39727007
Hullaah <umaradelo1.247@gmail.com>
2024-08-07 14:59:09 +0100

    Solved all mandatory tasks

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,5 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()]).then(([{ body }, { firstName, lastName }]) => {
     console.log(body, firstName, lastName);
-  });
+  }).catch(() => console.log('Signup system offline'));
 }
