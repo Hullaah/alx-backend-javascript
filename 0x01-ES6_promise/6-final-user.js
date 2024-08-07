576dc81e1dcbc1a853380a45e01cc83361013803
Hullaah <umaradelo1.247@gmail.com>
2024-08-07 15:28:59 +0100

    Fixed all isuues

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const arr = [
     signUpUser(firstName, lastName).then((value) => ({ status: 'fulfilled', value })),
-    uploadPhoto(fileName).catch((value) => ({ status: 'rejected', value })),
+    uploadPhoto(fileName).catch((err) => ({ status: 'rejected', value: err.toString() })),
   ];
   return Promise.all(arr);
 }
