20584d71aa76fb35b49fa37bb22fe7c7b0084df6
Hullaah <umaradelo1.247@gmail.com>
2024-08-15 22:57:32 +0100

    solved all es6 classes task

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from './3-currency';
+// import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -22,7 +22,11 @@ export default class Pricing {
     this._currency = value;
   }
 
-  displauFullPrice() {
-    return `${amount} ${this.currency.displauFullCurrency()}`
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
   }
 }
