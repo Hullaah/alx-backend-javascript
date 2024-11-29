a6778b1d3406b56959e086c054dc354974ed614f
Hullaah <umaradelo1.247@gmail.com>
2024-11-29 21:34:19 +0100

    refactored imports to work for earlier versions of nodejs

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,5 @@
-const http = require('node:http');
-const fs = require('node:fs');
+const http = require('http');
+const fs = require('fs');
 
 function countStudents(path, outStream) {
   fs.readFile(path, 'utf8', (err, data) => {
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs/promises';
+import fs from 'fs/promises';
 
 export default async function readDatabase(path) {
   const contents = await fs.readFile(path, 'utf8');
